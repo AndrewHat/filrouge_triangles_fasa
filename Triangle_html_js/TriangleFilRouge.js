899e510773f1bee043396206718e19573f85dda1
DIALLO <saliejallow1@gmail.com>
2021-04-12 16:35:24 +0200

    commentaires pour js html

--- Triangle_html_js/TriangleFilRouge.js
+++ Triangle_html_js/TriangleFilRouge.js
@@ -1,3 +1,5 @@
+/* creation d'une fonction triangle qui va permettre de gérer les actions 
+à effectuer après le clic sur le bouton valider*/
 
 function Triangle(){
 
@@ -7,19 +9,25 @@ var c = document.getElementById("c").value;
 
 if ( a <= 0 || b<=0 || c<= 0 ){
 	
+//gestion des nombres non valides 
+
 	alert("veuillez entrer des nombres strictement positifs");
 }
 	else{
+		
+// Indication de la nature du triangle en fonction de la longueur des cotés 
 
-if (a != b && b != c && c != a){
+		if (a != b && b != c && c != a){
 	window.open("Triangle_scalene.html");
-}
-if ((a == c) && (a == b) && (b == c)){
+		}
+		
+		if ((a == c) && (a == b) && (b == c)){
 	window.open("Triangle_equilateral.html");
-}
-if ((a == b && a != c) || (a == c && a != b) || (b == c && b != a)){
+		}
+		
+		if ((a == b && a != c) || (a == c && a != b) || (b == c && b != a)){
 	window.open("Triangle_Isocele.html");
-}
+		}
 
 }
 }
